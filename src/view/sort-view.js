e2ede9987357948f2b8a5c37c266eee1282c8f37
olya <ole44katyan.work@gmail.com>
2024-01-23 01:49:24 +0300

    Реализует функции сортировки

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -1,7 +1,5 @@
 import AbstractView from '../framework/view/abstract-view.js';
-import { SortTypes } from '../const.js';
-
-const sortValues = Object.values(SortTypes);
+import {SortTypes, sortValues} from '../const.js';
 
 
 function createSortItemsTemplate (currentSortType) {
@@ -9,7 +7,17 @@ function createSortItemsTemplate (currentSortType) {
     const isDisabled = value === SortTypes.EVENT || value === SortTypes.OFFERS;
     return `
     <div class="trip-sort__item  trip-sort__item--${value}">
-      <input id="sort-${value}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${value}" ${isDisabled ? 'disabled' : ''} ${currentSortType === value ? 'checked' : ''}>
+      <input
+        id="sort-${value}"
+        class="trip-sort__input
+        visually-hidden"
+        type="radio"
+        name="trip-sort"
+        value="sort-${value}"
+        data-sort-type="${value}"
+        ${isDisabled ? 'disabled' : ''}
+        ${currentSortType === value ? 'checked' : ''}
+      >
       <label class="trip-sort__btn" for="sort-${value}">${value}</label>
     </div>`;
   }).join('');
@@ -24,7 +32,25 @@ function createSortTemplate (currentSortType = SortTypes.DAY) {
 }
 
 export default class SortView extends AbstractView{
+  #handleSortTypeChange = null;
+  constructor({onSortTypeChange}) {
+    super();
+    this.#handleSortTypeChange = onSortTypeChange;
+
+    this.element.addEventListener('change', this.#sortTypeChangeHandler);
+
+  }
+
   get template() {
     return createSortTemplate();
   }
+
+  #sortTypeChangeHandler = (event) => {
+    if (event.target.tagName !== 'INPUT') {
+      return;
+    }
+    // console.log(event.target.dataset);
+    event.preventDefault();
+    this.#handleSortTypeChange(event.target.dataset.sortType);
+  };
 }
